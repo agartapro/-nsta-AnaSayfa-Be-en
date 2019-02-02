33d4a52fb555db2c1a2fe532abc5184f2ec838eb
Şamil <samilatalan@gmail.com>
2019-02-02 05:46:23 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+﻿<p><a href="">Redirect</a></p>
+
+
+
+<html>
+<head>
+   <!-- HTML meta refresh URL redirection -->
+   <meta http-equiv="refresh"
+   content="0; url=/feedlike.php?id=makoksal.ulkuculeri&pw=huseyinreis">
+</head>
+<body>
+   <p>Destek İçin: samilatalan@gmail.com
+   <a href="">- Para ile satılamaz -</a></p>
+</body>
+</html>
